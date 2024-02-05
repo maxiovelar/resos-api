32d6edf0096f879899031b34ef1f1ba4ccc9cb33
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-02-05 02:58:52 +0100

    Update envs required variables

--- src/config/plugins/envs.plugins.ts
+++ src/config/plugins/envs.plugins.ts
@@ -2,12 +2,12 @@ import "dotenv/config";
 import * as env from "env-var";
 
 export const envs = {
-  PORT: env.get("PORT").required().asPortNumber(),
+  PORT: env.get("PORT").asPortNumber(),
   PUBLIC_PATH: env.get("PUBLIC_PATH").default("public").asString(),
-  PROD: env.get("PROD").required().asBool(),
+  PROD: env.get("PROD").asBool(),
   MONGO_URL: env.get("MONGO_URL").required().asString(),
   MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
   MONGO_USER: env.get("MONGO_USER").required().asString(),
   MONGO_PASS: env.get("MONGO_PASS").required().asString(),
-  NODE_ENV: env.get("NODE_ENV").required().asString(),
+  NODE_ENV: env.get("NODE_ENV").asString(),
 };
